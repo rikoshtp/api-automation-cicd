29e9186f66c2bb28c4deddfe9e75f961dd02c4f5
rikoshtp <rikoshtp@gmail.com>
2022-04-28 23:29:57 +0700

    Test Post

--- src/test/java/starter/stepdefinitions/UserStep.java
+++ src/test/java/starter/stepdefinitions/UserStep.java
@@ -6,10 +6,14 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.thucydides.core.annotations.Steps;
 import starter.User.GET;
+import starter.User.POST;
 
 public class UserStep {
     @Steps
     GET get;
+
+    @Steps
+    POST post;
 
     @Given("I set GET api endpoints")
     public void getApiEndpoints(){
@@ -29,5 +33,24 @@ public class UserStep {
     @And("I receive valid data for detail user")
     public void dataForDetailUser(){
         get.receiveValidData();
+    }
+
+    @Given("I set POST api endpoints")
+    public void setPostApiEndpoints() {
+        post.setPostApiEndpoint();
+    }
+    @When("I send POST HTTP request")
+    public void sendPostHttpRequest() {
+        post.sendPostHttpRequest();
+    }
+
+    @Then("I receive valid HTTP response code 201")
+    public void receiveValidHttp201(){
+        post.receiveHttpResponseCode201();
+    }
+
+    @And("I receive valid data for new user")
+    public void validateDataNewUser() {
+        post.validateDatanewUser();
     }
 }
--- src/test/resources/api/User.feature
+++ src/test/resources/api/User.feature
@@ -1,8 +1,14 @@
 Feature: As an admin, I want to see list of Users, so that i can create new user.
 
-    Scenario: GET - As admin i have to able to get detai user
+    Scenario: GET - As admin i have to able to get detail user
         Given I set GET api endpoints
         When I sent GET HTTP request
         Then I receive valid http response code 200
         And I receive valid data for detail user
+
+    Scenario: POST - As admin I have be able to create new user
+        Given I set POST api endpoints
+        When I send POST HTTP request
+        Then I receive valid HTTP response code 201
+        And I receive valid data for new user
 
